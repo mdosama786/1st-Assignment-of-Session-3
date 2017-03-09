b6ace53afa319b9780fe4eaea47e499e0efc778e
Mohammad Osama <mdosama.ansar@gmail.com>
2017-03-09 17:26:27 +0530

    1st-Assignment-of-Session-3
    Answer .C
    
    This will run, print a message and terminate gracefully.Because the
    runtime system needs to load the class before it can look for the main
    method. So the static initializer will run first and print "10".
    Immediately after that System.exit(0) will  be called terminating the
    program before an error can be thrown.

---
+++ Assignment 3.1/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+	static {
+		print(10);
+	}
+
+	static void print(int x) {
+		System.out.println(x);
+		System.exit(0);
+	}
+}
+
+/* This will run, print a message and terminate gracefully.
+The runtime system needs to load the class before it can look
+for the main method. So the static initializer will run first
+and print "10". Immediately after that System.exit(0) will 
+be called terminating the program before an error can be thrown.
+*/
